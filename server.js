c0c6f76e0db27a004d9d7bf5b7443b3438b5b79a
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-04-17 12:30:37 -0400

    fixed node deploy

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/tictactoe.html');
+});
+
+app.listen((process.env.PORT || 8080), function () {
+  console.log('Tictactoe app listening on port!');
+});
